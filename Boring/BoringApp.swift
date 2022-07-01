374e034065b7c17b87b1cf8f4dc379263f0b5ffa
Deniz Mersinlioglu <mersinliogludeniz@gmai.com>
2022-07-01 12:00:16 +0300

    Initial Commit

---
+++ Boring/BoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoringApp.swift
+//  Boring
+//
+//  Created by Deniz Mersinlioğlu on 1.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
